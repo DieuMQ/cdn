db5eb39db9f31164e877cfbf74990472f14bf7cd
Dieu Mai Quang (CongNghe-HO) <dieu.maiquang@vndirect.com.vn>
2019-10-25 19:58:18 +0700

    add file noi that

---
+++ index-noithatchungcu_files/M1tNQFQSYrU.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["lSMnB"]); }
+
+__d("XPagesMessageCallToActionSendController",["XController"],(function(a,b,c,d,e,f){e.exports=b("XController").create("/pages/call_to_action/message_cta_send/",{page_id:{type:"Int",required:!0}})}),null);
